5bdc0f42a2fca74005174649d211cf885179f943
Justin Gent <justinbgent@gmail.com>
2019-08-16 16:25:00 -0600

    Incorporate color theme.

--- app/src/main/java/com/example/android_sprint2_challenge/ShoppingListAdapter.kt
+++ app/src/main/java/com/example/android_sprint2_challenge/ShoppingListAdapter.kt
@@ -10,6 +10,7 @@ import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_sprint2_challenge.model.ItemsList
+import com.example.android_sprint2_challenge.model.Values.Companion.myShoppingList
 import com.example.android_sprint2_challenge.model.Values.Companion.shoppingListStrings
 import kotlinx.android.synthetic.main.shopping_item_layout.view.*
 
@@ -24,23 +25,24 @@ class ShoppingListAdapter(private val data: MutableList<ItemsList>) : RecyclerVi
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val cardviewSwitch = Switch(holder.cardView.context)
-
         holder.itemName.text = data[position].item
         holder.iconView.setImageDrawable(ContextCompat.getDrawable(holder.iconView.context, data[position].icon))
 
+        val cardviewSwitch = Switch(holder.cardView.context)
         cardviewSwitch.isChecked = false
+
         holder.cardView.setOnClickListener {
             cardviewSwitch.isChecked = !cardviewSwitch.isChecked
             if (cardviewSwitch.isChecked){
                 shoppingListStrings.add(data[position].item)
                 holder.cardView.setCardBackgroundColor(ContextCompat.getColor(holder.cardView.context, R.color.colorPrimary))
+                ShoppingListAdapter(myShoppingList).notifyDataSetChanged()
             }else{
                 for (i in 0 until shoppingListStrings.size)
                     if (shoppingListStrings[i] == data[position].item){
                         shoppingListStrings.removeAt(i)
                     }
-                holder.cardView.setCardBackgroundColor(ContextCompat.getColor(holder.cardView.context, R.color.cardview_light_background))
+                holder.cardView.setCardBackgroundColor(ContextCompat.getColor(holder.cardView.context, R.color.colorAccent))
             }
         }
     }
